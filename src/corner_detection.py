9a9cbe28c92633b476124f5324ac92e6ba814be9
egears <ea.egearslan@gmail.com>
2016-12-03 13:40:03 -0500

    Hough Transform progress

--- src/corner_detection.py
+++ src/corner_detection.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as p
 from document_scanner import create_edge_image
-from scipy.ndimage.filters import maximum_filter
-from scipy.ndimage.morphology import generate_binary_structure, binary_erosion
+# from scipy.ndimage.filters import maximum_filter
+# from scipy.ndimage.morphology import generate_binary_structure, binary_erosion
 
 def gradient_direction(image):
 
@@ -58,36 +58,36 @@ def hough_transform(image):
 
     return accumulator, thetas, rhos, width
 
-def detect_peaks(image):
-    """
-    Takes an image and detect the peaks usingthe local maximum filter.
-    Returns a boolean mask of the peaks (i.e. 1 when
-    the pixel's value is the neighborhood maximum, 0 otherwise)
-    """
-
-    # define an 8-connected neighborhood
-    neighborhood = generate_binary_structure(2,2)
-
-    # apply the local maximum filter; all pixel of maximal value
-    # in their neighborhood are set to 1
-    local_max = maximum_filter(image, footprint=neighborhood)==image
-    # local_max is a mask that contains the peaks we are
-    # looking for, but also the background.
-    # In order to isolate the peaks we must remove the background from the mask.
-
-    # we create the mask of the background
-    background = (image == 0)
-
-    # a little technicality: we must erode the background in order to
-    # successfully subtract it form local_max, otherwise a line will
-    # appear along the background border (artifact of the local maximum filter)
-    eroded_background = binary_erosion(background, structure=neighborhood, border_value=1)
-
-    # we obtain the final mask, containing only peaks,
-    # by removing the background from the local_max mask (xor operation)
-    detected_peaks = local_max ^ eroded_background
-
-    return detected_peaks
+# def detect_peaks(image):
+#     """
+#     Takes an image and detect the peaks usingthe local maximum filter.
+#     Returns a boolean mask of the peaks (i.e. 1 when
+#     the pixel's value is the neighborhood maximum, 0 otherwise)
+#     """
+#
+#     # define an 8-connected neighborhood
+#     neighborhood = generate_binary_structure(2,2)
+#
+#     # apply the local maximum filter; all pixel of maximal value
+#     # in their neighborhood are set to 1
+#     local_max = maximum_filter(image, footprint=neighborhood)==image
+#     # local_max is a mask that contains the peaks we are
+#     # looking for, but also the background.
+#     # In order to isolate the peaks we must remove the background from the mask.
+#
+#     # we create the mask of the background
+#     background = (image == 0)
+#
+#     # a little technicality: we must erode the background in order to
+#     # successfully subtract it form local_max, otherwise a line will
+#     # appear along the background border (artifact of the local maximum filter)
+#     eroded_background = binary_erosion(background, structure=neighborhood, border_value=1)
+#
+#     # we obtain the final mask, containing only peaks,
+#     # by removing the background from the local_max mask (xor operation)
+#     detected_peaks = local_max ^ eroded_background
+#
+#     return detected_peaks
 
 
 def main():
@@ -101,19 +101,19 @@ def main():
 
     h, thetas, rhos, x2 = hough_transform(img)
 
-    print detect_peaks(h)
+    c = np.squeeze(np.where(h == h.max()))
 
-    # c = np.squeeze(np.where(h == h.max()))
-    #
-    # rho = rhos[c[0]]
-    # theta = thetas[c[1]]
-    #
-    # x1 = 0
-    # # if theta ==
-    # y1 = int(rho/sin(theta))
-    # y2 = int(x2*cos(theta) + y1)
-    #
-    # cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
+    rho = rhos[c[0]]
+    theta = thetas[c[1]]
+
+    x1 = 0
+    # if theta ==
+    y1 = int(rho/sin(theta))
+    y2 = int(x2*cos(theta) + y1)
+
+    print x1, y1, x2, y2
+
+    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
 
     stop = timeit.default_timer()
     # print stop - start
@@ -126,8 +126,6 @@ def main():
     p.figure()
     p.imshow(h)
     p.show()
-    np.savetxt('hough.txt', h)
-
 
 if __name__ == "__main__":
     main()
